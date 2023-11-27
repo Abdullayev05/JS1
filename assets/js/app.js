39c2af5202055ebdd61b71f31a97214528fd77ce
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2023-11-27 08:17:38 +0400

    mathrandom

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+// var name = prompt("Adin nedi?")
+// var surname = prompt("Soyadin nedi?")
+// var age = prompt("Nece yasin var?")
+// alert ("Salam" name + " " + surname)
+// console.log ("5 il sonra sizin 27 yasiniz")
+// var age = prompt("Nece yasin var?")
+// age 
+//      switch 
+// console.log (typeof age);
+// var months = age * 18;
+// console.log (months + " ay");
+// var age = prompt ("Neçə il ?");
+// console.log (age * 12);
+// console.log (age * 365);r
+// console.log (age * 365 * 24);
+// console.log (age * 365 * 24 * 60);
+// console.log (age * 365 * 60 * 60);
+// console.log (age * 365 * 60 * 60 * 1000);
+// var names = prompt ("What is your name ?")
+// if (names.length < 5) {
+//     console.log("kicik yas")
+// }
+// else if (names.length < 10) {
+//     console.log("orta yas")
+// }
+// else if ( names.length < 20) {
+//     console.log ("boyuk yas")
+// }
+
+// var trash = [
+//     17,
+//     "sagol"
+//     {
+//         name: "name",
+//     }
+//     true,
+//     12
+// ]
+// console.log(trash)
+
+
+console.log(Math.random) 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  console.log(getRandomInt(3));  
+  console.log(getRandomInt(1));  
+  console.log(Math.random());
+
+  var name = prompt("")
+
+

549a4ea452261bec0ad8ab4faee5a09c61d17f2e
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2023-12-14 12:56:46 +0400

    cut tek reqem

--- assets/js/app.js
+++ assets/js/app.js
@@ -352,38 +352,86 @@
 
 
 
-let mebleg = prompt("Pul miqdari yaz")
-let text = "";
-if (parseInt(mebleg / 500)) {
-    text += `${parseInt(mebleg / 500)} 500 manat, `;
-    mebleg = mebleg - 500 * parseInt (mebleg / 500)
+// let mebleg = prompt("Pul miqdari yaz")
+// let text = "";
+// if (parseInt(mebleg / 500)) {
+//     text += `${parseInt(mebleg / 500)} 500 manat, `;
+//     mebleg = mebleg - 500 * parseInt (mebleg / 500)
+// }
+// if (parseInt(mebleg / 200)) {
+//     text += `${parseInt(mebleg / 200)} 200 manat, `;
+//     mebleg = mebleg - 200 * parseInt (mebleg / 200)
+// }
+// if (parseInt(mebleg / 100)) {
+//     text += `${parseInt(mebleg / 100)} 100 manat, `;
+//     mebleg = mebleg - 100 * parseInt (mebleg / 100)
+// }
+// if (parseInt(mebleg / 50)) {
+//     text += `${parseInt(mebleg / 50)} 50 manat, `;
+//     mebleg = mebleg - 50 * parseInt (mebleg / 50)
+// }
+// if (parseInt(mebleg / 20)) {
+//     text += `${parseInt(mebleg / 20)} 20 manat, `;
+//     mebleg = mebleg - 20 * parseInt (mebleg / 20)
+// }
+// if (parseInt(mebleg / 10)) {
+//     text += `${parseInt(mebleg / 10)} 10 manat, `;
+//     mebleg = mebleg - 10 * parseInt (mebleg / 10)
+// }
+// if (parseInt(mebleg / 5)) {
+//     text += `${parseInt(mebleg / 5)} 5 manat, `;
+//     mebleg = mebleg - 5 * parseInt (mebleg / 5)
+// }
+// if (parseInt(mebleg / 1)) {
+//     text += `${parseInt(mebleg / 1)} 1 manat, `;
+//     mebleg = mebleg - 1 * parseInt (mebleg / 1)
+// }
+// console.log(text);
+
+
+// let numbers = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100"]
+
+// let cutReqem = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98];
+// let cut = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   cut += numbers[i];       
+// }
+// console.log(cut);
+// let tekReqem = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99];
+// let  = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// console.log(sum);
+
+
+// let nmbr = 0;
+// let number = 0;
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     number += i; 
+//   } else {
+//     nmbr += i; 
+//   }
+// }
+// console.log("Tek reqemler: " + nmbr);
+// console.log("Cut reqemler: " + number);
+
+
+let tekReqem = 0;
+let cutReqem = 0;
+let nmbr = "";
+let number = "";
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    cutReqem += i;
+    number += i + ",";
+  } else {
+    tekReqem += i;
+    nmbr += i + ",";
+  }
 }
-if (parseInt(mebleg / 200)) {
-    text += `${parseInt(mebleg / 200)} 200 manat, `;
-    mebleg = mebleg - 200 * parseInt (mebleg / 200)
-}
-if (parseInt(mebleg / 100)) {
-    text += `${parseInt(mebleg / 100)} 100 manat, `;
-    mebleg = mebleg - 100 * parseInt (mebleg / 100)
-}
-if (parseInt(mebleg / 50)) {
-    text += `${parseInt(mebleg / 50)} 50 manat, `;
-    mebleg = mebleg - 50 * parseInt (mebleg / 50)
-}
-if (parseInt(mebleg / 20)) {
-    text += `${parseInt(mebleg / 20)} 20 manat, `;
-    mebleg = mebleg - 20 * parseInt (mebleg / 20)
-}
-if (parseInt(mebleg / 10)) {
-    text += `${parseInt(mebleg / 10)} 10 manat, `;
-    mebleg = mebleg - 10 * parseInt (mebleg / 10)
-}
-if (parseInt(mebleg / 5)) {
-    text += `${parseInt(mebleg / 5)} 5 manat, `;
-    mebleg = mebleg - 5 * parseInt (mebleg / 5)
-}
-if (parseInt(mebleg / 1)) {
-    text += `${parseInt(mebleg / 1)} 1 manat, `;
-    mebleg = mebleg - 1 * parseInt (mebleg / 1)
-}
-console.log(text);
+nmbr = nmbr.slice(0, -1);
+number = number.slice(0, -1);
+console.log("Tek reqemlerin toplanma sayi " + tekReqem + " (" + nmbr + ")");
+console.log("Cut reqemin toplanma sayi " + cutReqem + " (" + number + ")");
